a742730d9c05cd943bb8a97742a3dad07a00793d
German Vazquez <germanalfredovazquez@gmail.com>
2022-08-08 16:42:32 -0300

    prueba 2

--- src/mesas/descartable.java
+++ src/mesas/descartable.java
@@ -2,4 +2,5 @@ package mesas;
 
 public class descartable {
     String frase= "esta es una clase solo para experimentar con git en vinculacion con intellij";
+    String frase2= "esta es la segunda linea. probemos esta huea...-";
 }
