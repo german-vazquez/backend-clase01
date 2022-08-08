5615378354d851c12e8a1e629da20f14dc8c72d7
german-vazquez <germanalfredovazquez@gmail.com>
2022-08-08 16:45:08 -0300

    prueba de cambios ultima

--- src/mesas/descartable.java
+++ src/mesas/descartable.java
@@ -3,4 +3,5 @@ package mesas;
 public class descartable {
     String frase= "esta es una clase solo para experimentar con git en vinculacion con intellij";
     String frase2= "esta es la segunda linea. probemos esta huea...-";
+    String frase3 =" esta es la ultima modificaci{on antes de borrar esta clase. ";
 }
